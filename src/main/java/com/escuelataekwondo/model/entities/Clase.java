bd483a3be22aa41cdd54c5467e0c2be1a3db2c69
Alan Peña <lanazirot@gmail.com>
2020-11-19 22:50:13 -0600

    Clase class added with attributes

---
+++ src/main/java/com/escuelataekwondo/model/entities/Clase.java
@@ -0,0 +1,16 @@
+package com.escuelataekwondo.model.entities;
+
+import java.util.Date;
+
+public class Clase {
+	//TODO ClaseBuilder at @see builder package
+	
+	private Date fecha;
+	private int idClase;
+	private int idEscuela;
+	private int duracion;
+	private String comentarios;
+	
+	
+	
+}
